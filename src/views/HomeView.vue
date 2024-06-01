c08838095296cf273501de97fec878f71406db75
Qing浅 <shengtong9828@163.com>
2024-06-02 06:43:52 +0800

    axios-api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+function test() {
+  axios.get('/api/helloworld').then((res) => {
+    console.log('/api/helloworld', res)
+  })
+}
+function test2() {
+  axios.post('/api/helloworld').then((res) => {
+    console.log('/api/helloworld', res)
+  })
+}
 </script>
 
 <template>
   <main>
+    <button @click="test">test</button>
+    <button @click="test2">test2</button>
     <TheWelcome />
   </main>
 </template>
